17a42ba419965521078594363ad7dd733fe25814
Tyler Thompson <studiesweekly@Tylers-MacBook-Pro.local>
2019-11-21 16:50:53 -0700

    edited getExpenses

--- db/getExpenses.sql
+++ db/getExpenses.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT id, date, name, amount, ischecked, notes, category, fulldate, userid, sum(amount) over (ORDER BY id) running_Total
 FROM homeexpenses
 WHERE userid = 1
-AND extract(month FROM fulldate) = 11
+AND extract(month FROM fulldate) = extract (month FROM CURRENT_DATE)
 AND extract(year FROM fulldate) = extract (year FROM CURRENT_DATE)
 ORDER BY id ASC;
