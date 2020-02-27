5dbdcfc1ead05d44123b25c739800a6543155ec4
TylerT <alpinetyler@gmail.com>
2020-02-26 19:58:23 -0700

    fixes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const massive = require('massive')
 const session = require('express-session')
 // const twilio = require('twilio');
 
-gi
+
 const ExpenseCtrl = require('./controllers/expenses')
 const CategoryCtrl = require('./controllers/categories')
 const MonthCtrl = require('./controllers/months')
