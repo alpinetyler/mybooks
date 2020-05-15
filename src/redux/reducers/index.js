e0a5c186ef8792c74c1bc23714ac9b0eeba13eb4
Tyler Thompson <studiesweekly@Tylers-MacBook-Pro.local>
2020-05-15 10:57:29 -0600

    Adding redux files

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+
+import user from './user'
+import month from './month'
+
+export default combineReducers({ user, month })
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux'
+import promiseMiddleware from 'redux-promise-middleware'
+
+import reducer from './reducers'
+
+export default 
+createStore(
+    reducer, applyMiddleware(promiseMiddleware)
+
+    )
+
+    // applyMiddleware(promiseMiddleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
