e9921fabf65a313ab06f64cd94cd66e74ebff811
TylerT <alpinetyler@gmail.com>
2020-05-20 20:13:10 -0600

    minor change

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
+
+
 export default class AddCategory extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            categoryname: []
+            categoryname: '',
+            userid: ''
         }
     }
 
