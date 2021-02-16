e941bb370b80fd0abc44b1fd26d7540058ce3839
Tyler Thompson <studiesweekly@Tylers-MacBook-Pro.local>
2021-02-15 19:43:20 -0700

    changes to css, update npm etc.

--- db/init/082119.sql
+++ db/init/082119.sql
@@ -1,36 +1,32 @@
 CREATE TABLE homeexpenses(
 id SERIAL PRIMARY KEY,
 name VARCHAR,
-date DATE,
+date VARCHAR,
 amount DECIMAL,
-ischecked BOOLEAN,
+ischecked VARCHAR,
 notes  VARCHAR,
 category VARCHAR,
-
+fulldate, DATE,
+userid, INTEGER
 );
+
 
 CREATE TABLE category(
     id SERIAL PRIMARY KEY,
-    categoryname VARCHAR
+    categoryname VARCHAR,
+    userid INTEGER
 );
 
 CREATE TABLE monthList(
     id SERIAL PRIMARY KEY,
     monthName VARCHAR,
     monthNumber INT,
-    years INT
+    years INT,
+    userid INT,
+    beginningbalance NUMERIC(15,7)
 );
 
-CREATE TABLE homeexpenses(
-id SERIAL PRIMARY KEY,
-name VARCHAR,
-date DATE,
-amount DECIMAL,
-ischecked BOOLEAN,
-notes  VARCHAR,
-category VARCHAR,
 
-);
 
 
 INSERT INTO homeexpenses
@@ -46,3 +42,6 @@ INSERT INTO homeexpenses
           category,
           ${userid}
       FROM fixedexpenses 
+
+-- How to See table structur: select column_name, data_type, character_maximum_length, column_default, is_nullable
+-- from INFORMATION_SCHEMA.COLUMNS where table_name = 'monthlist';
