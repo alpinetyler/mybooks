05f8fa6556bbb29386aebc8c352b58656201cff0
alpinetyler <49990880+alpinetyler@users.noreply.github.com>
2021-08-11 11:02:27 -0600

    save latest changes, ready to update origin master

---
+++ precache-manifest.f2d3f246fabc752f5315c4d3a710ce53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399e356ce2dd6cdf428d2a2f288d4b0f",
+    "url": "/mybooks/index.html"
+  },
+  {
+    "revision": "b37538735b736e8d29a9",
+    "url": "/mybooks/static/css/main.4aeb471a.chunk.css"
+  },
+  {
+    "revision": "e16807fbf3b65f31286d",
+    "url": "/mybooks/static/js/2.026cacdb.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/mybooks/static/js/2.026cacdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37538735b736e8d29a9",
+    "url": "/mybooks/static/js/main.11d94520.chunk.js"
+  },
+  {
+    "revision": "aeea23565e19e83ed2e0",
+    "url": "/mybooks/static/js/runtime-main.003de6ce.js"
+  }
+]);
